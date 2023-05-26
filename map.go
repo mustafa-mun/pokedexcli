8bc6f98e4a0b0074be5d5f079887fdc64cf48e9e
mustafa <mustafamcm9@gmail.com>
2023-05-26 21:52:03 +0300

    fix the pagination bug

--- map.go
+++ map.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"encoding/json"
 	"fmt"
@@ -11,14 +10,20 @@ import (
 
 
 type config struct {
-	limit int
 	offset int
 	next string
 	previous string
 }
 
 func commandMap(cfg *config) {
-	// start from page 0 (limit=0, offset=0)
+	// set current page to previous
+	cfg.previous = cfg.next
+	// Increment offset
+	cfg.offset += 20
+	// set next page
+	cfg.next = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v", cfg.offset)
+
+	// get next page
 	res, err := http.Get(cfg.next)
 	if err != nil {
 		log.Fatal(err)
@@ -34,20 +39,14 @@ func commandMap(cfg *config) {
 		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
 	}
 
+	// get result
 	var result map[string]interface{}
 	err = json.Unmarshal(body, &result)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Increment limit and offset
-	if cfg.limit + 20 <= int(result["count"].(float64)) {
-		cfg.limit += 20
-		cfg.offset += 20
-	}
-
-	cfg.next = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", cfg.limit, cfg.offset)
-
+  // get results array and print the location area
 	results := result["results"].([]interface{})
 	for _, r := range results {
 		location := r.(map[string]interface{})
@@ -58,7 +57,7 @@ func commandMap(cfg *config) {
 
 func commandMapb(cfg *config) {
 	// If user is not on the first page
-	if cfg.limit > 20 {
+	if cfg.offset > 0 {
 		res, err := http.Get(cfg.previous)
 		if err != nil {
 			log.Fatal(err)
@@ -74,17 +73,18 @@ func commandMapb(cfg *config) {
 			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
 		}
 
+		// decrease the page num
+		cfg.offset -= 20
+		cfg.offset -= 20
+
+		cfg.next = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v",cfg.offset)
+
+
 		var result map[string]interface{}
 		err = json.Unmarshal(body, &result)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		// Substract limit and offset
-			cfg.limit -= 20
-			cfg.offset -= 20
-
-		cfg.previous = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", cfg.limit, cfg.offset)
 
 		results := result["results"].([]interface{})
 		for _, r := range results {
