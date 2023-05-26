f310ca13726e13ec2e2559aea585c5dfb15f52b2
mustafa <mustafamcm9@gmail.com>
2023-05-26 21:50:03 +0300

    seperate the main repl loop

--- main.go
+++ main.go
@@ -1,41 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	limit := 0
-  offset := 0
-	cfg := config{
-		limit: limit,
-		offset: offset,
-		next:     fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", limit, offset),
-		previous: fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", limit, offset),
-	}
-	commands := returnCommands(commandHelp, commandClear, func(){commandMap(&cfg)}, func(){commandMapb(&cfg)})
-	// Begin the repl loop
-	reader := bufio.NewScanner(os.Stdin)
-	// commandHelp()
-	printPrompt()
-	for reader.Scan() {
-			// sanitize the input
-			text := cleanInput(reader.Text())
-			if command, exists := commands[text]; exists {
-					// Call commands callback function
-					command.callback()
-			} else if strings.EqualFold("exit", text) {
-					// Close the program on the exit command
-					return
-			} else {
-					// Invalid command, pass the command to the parser
-					handleCmd(text)
-			}
-			printPrompt()
-	}
-	// Print an additional line if we encountered an EOF character
-	fmt.Println()
+	cliLoop()	
 }
--- repl.go
+++ repl.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -64,4 +66,36 @@ func returnCommands(commandHelp, commandClear, commandMap, commandMapb func())(m
 				callback: commandClear,
 		},
 }
+}
+
+
+func cliLoop() {
+  offset := -20
+	cfg := config{
+		offset: offset,
+		next:     fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v", offset),
+		previous: fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v", offset),
+	}
+	commands := returnCommands(commandHelp, commandClear, func(){commandMap(&cfg)}, func(){commandMapb(&cfg)})
+	// Begin the repl loop
+	reader := bufio.NewScanner(os.Stdin)
+	// commandHelp()
+	printPrompt()
+	for reader.Scan() {
+			// sanitize the input
+			text := cleanInput(reader.Text())
+			if command, exists := commands[text]; exists {
+					// Call commands callback function
+					command.callback()
+			} else if strings.EqualFold("exit", text) {
+					// Close the program on the exit command
+					return
+			} else {
+					// Invalid command, pass the command to the parser
+					handleCmd(text)
+			}
+			printPrompt()
+	}
+	// Print an additional line if we encountered an EOF character
+	fmt.Println()
 }
