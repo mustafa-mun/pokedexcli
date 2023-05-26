f82400c35ddf4dbe7c82eba8040bc19e5820b6de
mustafa <mustafamcm9@gmail.com>
2023-05-26 20:03:58 +0300

    add map and mapb commands

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
@@ -27,10 +31,98 @@ func commandHelp() {
 			cliName,
 	)
 	fmt.Println("help    - Show available commands")
+	fmt.Println("map   - Show the names of 20 location areas")
+	fmt.Println("mapb   - Show previous names of 20 location areas")
 	fmt.Println("clear   - Clear the terminal screen")
 	fmt.Println("exit    - Closes your connection")
 }
 
+type config struct {
+	limit int
+	offset int
+	next string
+	previous string
+}
+
+func commandMap(cfg *config) {
+	// start from page 0 (limit=0, offset=0)
+	res, err := http.Get(cfg.next)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Increment limit and offset
+	if cfg.limit + 20 <= int(result["count"].(float64)) {
+		cfg.limit += 20
+		cfg.offset += 20
+	}
+
+	cfg.next = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", cfg.limit, cfg.offset)
+
+	results := result["results"].([]interface{})
+	for _, r := range results {
+		location := r.(map[string]interface{})
+		fmt.Println(location["name"])
+	}
+}
+
+
+func commandMapb(cfg *config) {
+	// If user is not on the first page
+	if cfg.limit > 20 {
+		res, err := http.Get(cfg.previous)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if res.StatusCode > 299 {
+			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		}
+
+		var result map[string]interface{}
+		err = json.Unmarshal(body, &result)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Substract limit and offset
+			cfg.limit -= 20
+			cfg.offset -= 20
+
+		cfg.previous = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", cfg.limit, cfg.offset)
+
+		results := result["results"].([]interface{})
+		for _, r := range results {
+			location := r.(map[string]interface{})
+			fmt.Println(location["name"])
+		}
+	} else {
+		fmt.Println("You are on the first page!")
+	}
+	
+}
 
 // clearScreen clears the terminal screen
 func commandClear() {
@@ -39,13 +131,23 @@ func commandClear() {
 	cmd.Run()
 }
 
-func returnCommands(commandHelp, commandClear func())(map[string]cliCommand) {
+func returnCommands(commandHelp, commandClear, commandMap, commandMapb func())(map[string]cliCommand) {
 	return map[string]cliCommand{
     "help": {
         name:        "help",
         description: "Displays a help message",
         callback:    commandHelp,
     },
+		"map": {
+			name:        "map",
+			description: "Displays the names of 20 location areas in the Pokemon world",
+			callback:    commandMap,
+	},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous names of 20 location areas in the Pokemon world",
+			callback:    commandMapb,
+	},
 		"clear" :{
 				name: "clear",
 				description: "Clears the terminal",
@@ -77,10 +179,18 @@ func cleanInput(text string) string {
 }
 
 func main() {
-	commands := returnCommands(commandHelp, commandClear)
+	limit := 0
+  offset := 0
+	cfg := config{
+		limit: limit,
+		offset: offset,
+		next:     fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", limit, offset),
+		previous: fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=%v&offset=%v", limit, offset),
+	}
+	commands := returnCommands(commandHelp, commandClear, func(){commandMap(&cfg)}, func(){commandMapb(&cfg)})
 	// Begin the repl loop
 	reader := bufio.NewScanner(os.Stdin)
-	commandHelp()
+	// commandHelp()
 	printPrompt()
 	for reader.Scan() {
 			// sanitize the input
