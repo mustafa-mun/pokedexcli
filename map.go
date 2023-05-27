798b067d1eece86c9d92797542a42804c9f1a521
mustafa <mustafamcm9@gmail.com>
2023-05-27 11:15:46 +0300

    little refactor on map functions   Get request to pokeapi is common with our two functions. This commit adds new function to avoid repeated code

--- map.go
+++ map.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 )
 
-
 type config struct {
 	prevOffset int
 	nextOffset int
@@ -16,21 +15,8 @@ type config struct {
 	previous string
 }
 
-/*
-FIX THE MAPPING BUG
-*/
-
-func commandMap(cfg *config) {
-	// set current page to previous
-	cfg.prevOffset = cfg.nextOffset
-	cfg.previous = cfg.next
-	// Increment offset
-	cfg.nextOffset += 20
-	// set next page
-	cfg.next = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v", cfg.nextOffset)
-
-	// get next page
-	res, err := http.Get(cfg.next)
+func getLocationAreas(url string) {
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -60,46 +46,29 @@ func commandMap(cfg *config) {
 	}
 }
 
+func commandMap(cfg *config) {
+	// set current page to previous
+	cfg.prevOffset = cfg.nextOffset
+	cfg.previous = cfg.next
+	// Increment offset
+	cfg.nextOffset += 20
+	// set next page
+	cfg.next = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v", cfg.nextOffset)
+	// get next page
+	getLocationAreas(cfg.next)
+}
 
 func commandMapb(cfg *config) {
 	// If user is not on the first page
 	if cfg.prevOffset >= 0 {
-		res, err := http.Get(cfg.previous)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer res.Body.Close()
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if res.StatusCode > 299 {
-			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-		}
-
+		getLocationAreas(cfg.previous)
 		// decrease the page num
 		cfg.nextOffset -= 20
 		cfg.prevOffset -= 20
 		// set prev and next to decreased offsets
 		cfg.previous = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v",cfg.prevOffset)
 		cfg.next = fmt.Sprintf("https://pokeapi.co/api/v2/location-area/?limit=20&offset=%v",cfg.nextOffset)
-
-
-		var result map[string]interface{}
-		err = json.Unmarshal(body, &result)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		results := result["results"].([]interface{})
-		for _, r := range results {
-			location := r.(map[string]interface{})
-			fmt.Println(location["name"])
-		}
 	} else {
 		fmt.Println("You are on the first page!")
 	}
-	
 }
